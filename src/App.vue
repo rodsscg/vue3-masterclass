697c6bdff5709be6b87a2b68a79d6ef21de92741
rodsscg <rodrigosantoscg@gmail.com>
2022-12-16 17:53:50 -0400

    feat: thread list

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,22 @@
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <home-page />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/style.css";
 </style>
