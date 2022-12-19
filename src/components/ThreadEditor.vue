e41013ce3dc262934daf665ea088d18cdaf5578e
rodsscg <rodrigosantoscg@gmail.com>
2022-12-18 22:34:37 -0400

    feat: create thread editor

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,54 @@
+<template>
+  <form
+    @reset.prevent="$emit('reset')"
+    @submit.prevent="$emit('submit', { title, text })"
+  >
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input
+        id="thread_title"
+        v-model="title"
+        type="text"
+        class="form-input"
+        name="title"
+      >
+    </div>
+
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea
+        id="thread_content"
+        v-model="text"
+        class="form-input"
+        name="content"
+        rows="8"
+        cols="140"
+      />
+    </div>
+
+    <div class="btn-group">
+      <button
+        type="reset"
+        class="btn btn-ghost"
+      >
+        Cancel
+      </button>
+      <button
+        class="btn btn-blue"
+        type="submit"
+        name="Publish"
+      >
+        Publish
+      </button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue'
+
+defineEmits(['reset', 'submit'])
+
+let title = ''
+let text = ''
+</script>
--- src/views/ThreadCreate.vue
+++ src/views/ThreadCreate.vue
@@ -1,50 +1,10 @@
 <template>
   <div class="col-full push-top">
     <h1>Create new thread in <i>{{ forum.name }}</i></h1>
-
-    <form
-      @reset.prevent="onReset"
-      @submit.prevent="onSubmit"
-    >
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input
-          id="thread_title"
-          v-model="title"
-          type="text"
-          class="form-input"
-          name="title"
-        >
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          id="thread_content"
-          v-model="text"
-          class="form-input"
-          name="content"
-          rows="8"
-          cols="140"
-        />
-      </div>
-
-      <div class="btn-group">
-        <button
-          type="reset"
-          class="btn btn-ghost"
-        >
-          Cancel
-        </button>
-        <button
-          class="btn btn-blue"
-          type="submit"
-          name="Publish"
-        >
-          Publish
-        </button>
-      </div>
-    </form>
+    <thread-editor
+      @reset="onReset"
+      @submit="onSubmit"
+    />
   </div>
 </template>
 
@@ -52,6 +12,8 @@
 import { computed, defineProps } from 'vue'
 import { useRouter } from 'vue-router';
 import { useStore } from 'vuex'
+
+import ThreadEditor from '@/components/ThreadEditor.vue'
 
 const router = useRouter()
 const { dispatch, state } = useStore()
@@ -63,16 +25,13 @@ const props = defineProps({
   },
 })
 
-let title = ''
-let text = ''
-
 const forum = computed(() => state.forums.find(f => f.id === props.forumId))
 
 const onReset = () => {
   router.push({ name: 'Forum', params: { id: props.forumId } })
 }
 
-const onSubmit = async () => {
+const onSubmit = async ({ title, text }) => {
   const thread = await dispatch('createThread', { title, text, forumId: props.forumId })
   router.push({ name: 'Thread', params: { id: thread.id } })
 }
