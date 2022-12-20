d06f4f3de87830e7b19b3d8459c84d477878ca4c
rodsscg <rodrigosantoscg@gmail.com>
2022-12-19 21:39:49 -0400

    feat: integrates firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyCbLe66Y5MsZ9oe2fStYpNWJCnWq1Y2NQI",
+  authDomain: "vue-school-forum-e5573.firebaseapp.com",
+  projectId: "vue-school-forum-e5573",
+  storageBucket: "vue-school-forum-e5573.appspot.com",
+  messagingSenderId: "173837107319",
+  appId: "1:173837107319:web:25064560ca52e2e26e4672"
+}
