cba3b1c8de787e46a70961846b46a1b488c62c31
rodsscg <rodrigosantoscg@gmail.com>
2022-12-26 11:21:53 -0400

    fix: threadShow

--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -41,46 +41,27 @@ import { computed, defineProps } from 'vue'
 import { useStore } from 'vuex'
 
 import { filterIn } from '@/helpers'
-
 import BaseDate from '@/components/BaseDate.vue'
 import PostEditor from '@/components/PostEditor.vue'
 import PostList from '@/components/PostList.vue'
 
 const { dispatch, getters, state} = useStore()
 
-const props = defineProps({
-  id: { type: String, required: true}
-})
+const props = defineProps({ id: { type: String, required: true} })
 
 const posts = computed(() => state.posts)
 const thread = computed(() => getters.thread(props.id))
 const threadPosts = computed(() => filterIn(posts.value).where('threadId', props.id))
-const users = computed(() => state.users)
-
-const loadData = async () => {
-  const threadData = await getThreadData()
-
-  if (threadData) {
-    getUserData(threadData.userId)
-    getThreadPosts()
-  }
-}
+const users = computed(() => state.users)  
 
 const getThreadData = () => dispatch('fetchThread', { id: props.id })
+const getThreadPosts = (thread) => dispatch('fetchPosts', { ids: thread.posts })
+const getUserIds = (posts) => Promise.resolve(posts.map(post => post.userId).concat(thread.value.userId))
+const getUsers = (ids) => dispatch('fetchUsers', { ids })
+const onSavePost = (eventData) => dispatch('createPost', { ...eventData.post, threadId: props.id })
 
-const getUserData = (id) => dispatch('fetchUser', { id })
-
-const getThreadPosts = async () => {
-  const posts = await dispatch('fetchPosts', { ids: thread.value.posts })
-  const users = posts.map(post => post.userId)
-
-  dispatch('fetchUsers', { ids: users })
-}
-
-const onSavePost = (eventData) => {
-  const post = { ...eventData.post, threadId: props.id}
-  dispatch('createPost', post)
-}
-
-loadData()
+getThreadData()
+  .then(getThreadPosts)
+  .then(getUserIds)
+  .then(getUsers)
 </script>
